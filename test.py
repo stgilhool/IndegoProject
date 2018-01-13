35477c035fd51be7e405c700a5475c3a6115fa3d
Steve <gilhool@sas.upenn.edu>
2018-01-12 23:33:31 -0500

    Using to test global vars behavior, forgot what I was testing tho

--- test.py
+++ test.py
@@ -3,7 +3,12 @@ import matplotlib.pyplot as pyplot
 import numpy as np
 import pandas as pd
 
-def steves_a_dick(verb,noun):
-    print('Hey Steve, you {0} like a {1}')
+table = np.arrange(6)
+
+def test(x):
+    table.appe
+    return x
+
+test(table)
 
 steves_a_dick('smell', 'dried up stinky dicklicker')
